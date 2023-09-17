e808323ea0fd8e070735a4db534ab2687298bb23
wrkabdo <wrkabdo@gmail.com>
2023-09-17 20:52:32 +0100

    hello

---
+++ projet 1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int m ;
+	printf("saisaier votre age:\n");
+	scanf("%i",&m);
+	printf("votre age est : %i",m);
+	return 1;
+}
---
+++ projet2/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	
+	int n;
+	printf("================================================================================= \t \t \n");
+    printf("Menu d''affichage \n");
+	printf("================================================================================= \n");
+	
+    printf(" [1] afficher les donnees .\n [2] ajouter les donnees .\n [3] rechercher sur une donnees .\n [4] supprimer une donnee .\n [5] supprimer les donnees repetes .\n [6] genere le wordlist .\n [7]genere le wordlist dans une ficher.txt .\n [8] afficher les donnees de type entier .\n ");	
+
+	printf("================================================================================= \n");
+	
+	printf("Tapez votre choix [1-6]:\n");
+     scanf("%i",&n);
+
+	
+	
+	
+	return 0;
+}
